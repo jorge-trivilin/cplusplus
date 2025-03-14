0eb2cc4133053862b1c3e067b5f87a4405288a78
Jorge Trivilin <jorge.filho@ab-inbev.com>
2025-03-13 22:49:48 -0300

    update

---
+++ Structure of a program/hello_world.cpp
@@ -0,0 +1,10 @@
+// my first program in C++
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	std::cout << "Hello World!\n"; // prints Hello World
+	std::cout << "I'm a C++ program\n"; // prints I'm a c++ PROGRAM
+	cout << "Testing using namespace std\n";
+}
